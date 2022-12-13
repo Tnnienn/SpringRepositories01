61bb3012ba7195abbdb0ff2e5ee521aeed5e6053
tnnienn <tnnienn02@gmail.com>
2022-12-13 23:42:04 +0100

    exercise implemented

--- src/main/java/co/develhope/springrepositories1/entities/Car.java
+++ src/main/java/co/develhope/springrepositories1/entities/Car.java
@@ -1,4 +1,25 @@
 package co.develhope.springrepositories1.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(nullable = false)
+    private String modelName;
+    @Column(nullable = false)
+    private String serialNumber;
+    private Double price;
+
 }
